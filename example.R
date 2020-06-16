e9ecebd3f4dfc22d99fa4cb2b5a4f114c7b85d8b
Roberto <robetatis@yahoo.com>
2020-06-16 19:40:53 +0200

    adjusted paths in example

--- example.R
+++ example.R
@@ -1,18 +1,18 @@
-source('c:/gitRepositories/riverPollution/probInterf.R')
-probInterf(latQpath='c:/gitRepositories/riverPollution/latQ.txt',
-           latQxPath='c:/gitRepositories/riverPollution/latQx.txt',
-           reachInfoPath='c:/gitRepositories/riverPollution/reachInfo.txt',
+source('......../probInterf.R')
+probInterf(latQpath='...../latQ.txt',
+           latQxPath='...../latQx.txt',
+           reachInfoPath='...../reachInfo.txt',
            latQerror=c(1.3, 0.32),
-           sampleTimesPath='c:/gitRepositories/riverPollution/sampleTimes.txt',
-           outTable='c:/gitRepositories/riverPollution/probInterf.txt',
+           sampleTimesPath='....../sampleTimes.txt',
+           outTable='...../probInterf.txt',
            dateTimeFormat='%m/%d/%Y %H:%M')
 
-source('c:/gitRepositories/riverPollution/lagrComp.R')
-lagrangianComparison(reachInfoPath='c:/gitRepositories/riverPollution/reachInfo.txt',
-                     sampleTimesPath='c:/gitRepositories/riverPollution/sampleTimes.txt',
-                     sampleConcentrationsPath='c:/gitRepositories/riverPollution/sampleConcentrations.txt',
-                     pInterfPath='c:/gitRepositories/riverPollution/probInterf.txt',
-                     outTable='c:/gitRepositories/riverPollution/kCond_1-2.txt',
+source('........./lagrComp.R')
+lagrangianComparison(reachInfoPath='..../reachInfo.txt',
+                     sampleTimesPath='...../sampleTimes.txt',
+                     sampleConcentrationsPath='....../sampleConcentrations.txt',
+                     pInterfPath='...../probInterf.txt',
+                     outTable='...../kCond_1-2.txt',
                      dateTimeFormat='%m/%d/%Y %H:%M',
                      stationUp=1,
                      stationDown=2)
